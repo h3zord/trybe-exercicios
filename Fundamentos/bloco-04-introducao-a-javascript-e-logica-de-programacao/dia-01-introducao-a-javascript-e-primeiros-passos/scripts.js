a0fec3182c9856ea9f440b07d3c723e26359148c
Lucas <lucaschmain@gmail.com>
2022-04-06 19:16:47 -0300

    descobrindo salario liquido

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-a-javascript-e-primeiros-passos/scripts.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-a-javascript-e-primeiros-passos/scripts.js
@@ -138,18 +138,43 @@ if (o % 2 != 0 || p % 2 != 0 || q % 2 != 0) {
     console.log("false")
 }
 
-const custo = 100
-const venda = 150
-const unidades = 1000
-const custoTotal = custo * 1.2 * unidades
-const valorVenda = venda * unidades
+const custo = 100;
+const venda = 150;
 
-switch (unidades) {
-    case 1000:
-        console.log(valorVenda - custoTotal);
-        break;    
+if (custo >= 0 && venda >= 0) {
+    const custoTotal = custo * 1.2;
+    const totalVenda = (venda - custoTotal) * 1000;
+    console.log(totalVenda)
 }
 
 
+let aliquotINSS;
+let aliquotIR;
 
+const grossSalary = 2000.00;
 
+if (grossSalary <= 1556.94) {
+  aliquotINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+  aliquotINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+  aliquotINSS = grossSalary * 0.11;
+} else {
+  aliquotINSS = 570.88;
+}
+
+const baseSalary = grossSalary - aliquotINSS;
+
+if (baseSalary <= 1903.98) {
+  aliquotIR = 0;
+} else if (baseSalary <= 2826.65) {
+  aliquotIR = (baseSalary * 0.075) - 142.80;
+} else if (baseSalary <= 3751.05) {
+  aliquotIR = (baseSalary * 0.15) - 354.80;
+} else if (baseSalary <= 4664.68) {
+  aliquotIR = (baseSalary * 0.225) - 636.13;
+} else {
+  aliquotIR = (baseSalary * 0.275) - 869.36;
+};
+
+console.log("Salário: " + (baseSalary - aliquotIR));
