b35cc6ece05de35c310f501ed1a764ea85057ca2
Lucas <lucaschmain@gmail.com>
2022-04-07 15:01:57 -0300

    fazendo a soma de todos os elementos do array

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -1,5 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (index = 0; index < numbers.length; index+= 1) {
+for (let index = 0; index < numbers.length; index+= 1) {
     console.log(numbers[index])
 }
+
+
+let somaNumbers = 0
+
+for (let index = 0; index < numbers.length; index+= 1) {
+    somaNumbers = somaNumbers + numbers[index];
+    
+}
+
+console.log(somaNumbers);
+
