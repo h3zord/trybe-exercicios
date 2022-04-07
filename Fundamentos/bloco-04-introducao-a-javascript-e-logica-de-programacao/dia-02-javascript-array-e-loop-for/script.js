6d878f1c0df483f543f9e6e70cefd2b6ee7ca452
Lucas <lucaschmain@gmail.com>
2022-04-07 15:57:21 -0300

    encontrando o menor numero do array

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -59,35 +59,35 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 
 
-// let menorValor;
+let menorValor = numbers[0]
 
-// for (let index = 0; index < numbers.length; index += 1) {
+for (let index = 0; index < numbers.length; index += 1) {
 
-//     if (menorValor > numbers[index]) {
-//         menorValor = numbers[index]
-//     } else {
-//         menorValor = menorValor
-//     }
+    if (menorValor > numbers[index]) {
+        menorValor = numbers[index]
+    } else {
+        menorValor = menorValor
+    }
 
+}
+
+console.log(menorValor);
+
+
+
+
+// let box = [];
+
+// for (index = 1; index < 26; index += 1) {
+//     box.push(index)
 // }
 
-// console.log(menorValor);
+// console.log(box)
 
-
-
-
-let box = [];
-
-for (index = 1; index < 26; index += 1) {
-    box.push(index)
-}
-
-console.log(box)
-
-for (contador = 0; contador < box.length; contador += 1) {
-    let resultadoDaDiv = box[contador] / 2;
-    console.log(resultadoDaDiv);
-}
+// for (contador = 0; contador < box.length; contador += 1) {
+//     let resultadoDaDiv = box[contador] / 2;
+//     console.log(resultadoDaDiv);
+// }
 
 
 
