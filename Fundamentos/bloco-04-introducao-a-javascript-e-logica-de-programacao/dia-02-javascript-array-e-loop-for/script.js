230f739573f91220341a9701d9fd206c0e5c6f55
Lucas <lucaschmain@gmail.com>
2022-04-07 15:53:09 -0300

    criando array e imprimindo resultado

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -56,16 +56,39 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // console.log(valoresImpares)
 
-let menorValor;
 
-for (let index = 0; index < numbers.length; index += 1) {
 
-    if (menorValor > numbers[index]) {
-        menorValor = numbers[index]
-    } else {
-        menorValor = menorValor
-    }
 
+// let menorValor;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+
+//     if (menorValor > numbers[index]) {
+//         menorValor = numbers[index]
+//     } else {
+//         menorValor = menorValor
+//     }
+
+// }
+
+// console.log(menorValor);
+
+
+
+
+let box = [];
+
+for (index = 1; index < 26; index += 1) {
+    box.push(index)
 }
 
-console.log(menorValor);
+console.log(box)
+
+for (contador = 0; contador < box.length; contador += 1) {
+    let resultadoDaDiv = box[contador] / 2;
+    console.log(resultadoDaDiv);
+}
+
+
+
+
