f4a7d177055e5b21fa2d06f86c78a4395cc4bbb4
Lucas <lucaschmain@gmail.com>
2022-04-07 15:08:39 -0300

    encontrando media aritmetica do array

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -1,16 +1,25 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index < numbers.length; index+= 1) {
-    console.log(numbers[index])
-}
+// for (let index = 0; index < numbers.length; index+= 1) {
+//     console.log(numbers[index])
+// }
 
 
-let somaNumbers = 0
+// let somaNumbers = 0
 
-for (let index = 0; index < numbers.length; index+= 1) {
-    somaNumbers = somaNumbers + numbers[index];
+// for (let index = 0; index < numbers.length; index+= 1) {
+//     somaNumbers = somaNumbers + numbers[index];
     
-}
+// }
 
-console.log(somaNumbers);
+// console.log(somaNumbers);
 
+let somaNumbers = 0;
+
+for (let index = 0; index < numbers.length; index+= 1) {
+
+     somaNumbers = somaNumbers + numbers[index];
+
+    }
+    
+    console.log(somaNumbers / numbers.length)
