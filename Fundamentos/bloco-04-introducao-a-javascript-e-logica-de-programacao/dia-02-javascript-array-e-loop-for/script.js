63ee24ff2e8c875414fea234b6ab5123ddfbab18
Lucas <lucaschmain@gmail.com>
2022-04-07 15:13:14 -0300

    descobrindo se a media e maior que 20

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -14,12 +14,21 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // console.log(somaNumbers);
 
-let somaNumbers = 0;
+// let somaNumbers = 0;
 
-for (let index = 0; index < numbers.length; index+= 1) {
+// for (let index = 0; index < numbers.length; index+= 1) {
 
-     somaNumbers = somaNumbers + numbers[index];
+//      somaNumbers = somaNumbers + numbers[index];
 
-    }
+//     }
     
-    console.log(somaNumbers / numbers.length)
+//     let mediaArit;
+//     mediaArit = somaNumbers / numbers.length;
+//     console.log(mediaArit);
+
+//     if (mediaArit > 20) {
+//         console.log("Valor maior que 20")
+//     } else {
+//         console.log("Valor menos que 20")
+//     }
+
