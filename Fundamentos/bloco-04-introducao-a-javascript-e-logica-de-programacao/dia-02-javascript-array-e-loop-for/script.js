420273f3dadb3cb10b9329c33f9a9d84317522b1
Lucas <lucaschmain@gmail.com>
2022-04-07 15:58:03 -0300

    ultimo save

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -59,19 +59,19 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 
 
-let menorValor = numbers[0]
+// let menorValor = numbers[0]
 
-for (let index = 0; index < numbers.length; index += 1) {
+// for (let index = 0; index < numbers.length; index += 1) {
 
-    if (menorValor > numbers[index]) {
-        menorValor = numbers[index]
-    } else {
-        menorValor = menorValor
-    }
+//     if (menorValor > numbers[index]) {
+//         menorValor = numbers[index]
+//     } else {
+//         menorValor = menorValor
+//     }
 
-}
+// }
 
-console.log(menorValor);
+// console.log(menorValor);
 
 
 
