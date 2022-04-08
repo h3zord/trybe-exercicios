720532cc216b663f4fd066947dc5facb8f629b4b
Lucas <lucaschmain@gmail.com>
2022-04-08 15:34:50 -0300

    algoritmo que inverte palavras

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,10 +1,23 @@
-let fatorial = 10;
-let resultado = 1;
+// let fatorial = 10;
+// let resultado = 1;
 
 
-for (let index = 1; index <= fatorial; index += 2) {
-    resultado = resultado * (index * (index +1))
+// for (let index = 1; index <= fatorial; index += 2) {
+//     resultado = resultado * (index * (index +1))
 
-}
+// }
 
-console.log(resultado);
+// console.log(resultado);
+
+
+
+// let word = "Tryber";
+// let invertida = [];
+
+// for (index = word.length - 1; index >= 0; index -= 1) {
+//     invertida.push(word[index])
+// }
+
+// console.log(invertida);
+
+
