98ac381b14887f93c47730863e482680662e9f19
Lucas <lucaschmain@gmail.com>
2022-04-11 15:01:29 -0300

    imprimindo nome do personagem e adicionando nova propriedade

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,9 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+  };
+
+  console.log("Bem-Vinda, " + info["personagem"]);
+  console.log(info)
