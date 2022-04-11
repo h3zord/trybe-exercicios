a9eef20de4b33202e432e75b870b73539dc632ac
Lucas <lucaschmain@gmail.com>
2022-04-11 15:19:47 -0300

    imprimindo valores de cada objeto

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
@@ -5,6 +5,14 @@ let info = {
     recorrente: 'Sim',
   };
 
- for (let index in info) {
-     console.log(info[index]);
- }
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  }
+
+  console.log(info.personagem + " e " + info2.personagem);
+  console.log(info['origem'] + " e " + info2['origem']);
+  console.log(info.nota + " e " + info2.nota);
+  console.log('Ambos recorrentes');
