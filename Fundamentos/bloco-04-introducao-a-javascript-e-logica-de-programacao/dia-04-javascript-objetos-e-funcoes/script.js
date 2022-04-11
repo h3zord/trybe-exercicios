cdfba77bf8202284a3bdc3fabe9b44a7f1706612
Lucas <lucaschmain@gmail.com>
2022-04-11 15:37:46 -0300

    acessando as chaves de de um array do objeto

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
@@ -1,18 +1,14 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim',
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
   };
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
-  }
-
-  console.log(info.personagem + " e " + info2.personagem);
-  console.log(info['origem'] + " e " + info2['origem']);
-  console.log(info.nota + " e " + info2.nota);
-  console.log('Ambos recorrentes');
+  console.log('O livro favorito de ' + leitor.nome + " " + leitor.sobrenome + ' se chama ' + "'" + leitor.livrosFavoritos[0].titulo + "'") 
