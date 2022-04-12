45877a471a238978da8363f501c6d80a46573399
Lucas <lucaschmain@gmail.com>
2022-04-12 12:51:31 -0300

    encontrando o indice do menor valor

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
@@ -10,18 +10,34 @@
 
 // console.log(verificaPalind('arara'));
 
-function maiorIndice (array) {
-    let maiorNumero = 0;
-    let indice = 0;
-    for (let index in array) {
-        if (array[index] > maiorNumero) {
-            maiorNumero = array[index]
-            indice = index
+// function maiorIndice (array) {
+//     let maiorNumero = 0;
+//     let indice = 0;
+//     for (let index in array) {
+//         if (array[index] > maiorNumero) {
+//             maiorNumero = array[index]
+//             indice = index
             
-        }
-    }
+//         }
+//     }
 
-    return indice
-}
+//     return indice
+// }
 
-console.log(maiorIndice([2, 3, 6, 7, 10, 1, 98]))
+// console.log(maiorIndice([2, 3, 6, 7, 10, 1, 98]))
+
+// function maiorIndice (array) {
+//     let maiorNumero = 0;
+//     let indice = 0;
+//     for (let index in array) {
+//         if (array[index] < maiorNumero) {
+//             maiorNumero = array[index]
+//             indice = index
+            
+//         }
+//     }
+
+//     return indice
+// }
+
+// console.log(maiorIndice([2, 4, 6, 7, 10, 0, -3]))
