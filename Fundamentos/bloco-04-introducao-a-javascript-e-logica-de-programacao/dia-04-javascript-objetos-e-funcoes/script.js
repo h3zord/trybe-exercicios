0ac161e00d745fa35045d9f7c698f7448b33c9e0
Lucas <lucaschmain@gmail.com>
2022-04-12 12:07:11 -0300

    verificando se e palindromo

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
@@ -1,33 +1,11 @@
-// let storage = '';
-
-// function verificaPalind (palavra) {
-//     let teste = palavra
-//     for (let i = teste.length -1; i >= 0; i -= 1); {
-//             storage = storage + teste[i];
-
-//     }
-//             if (teste == storage) {
-//                 console.log("true")
-//             } else {
-//                 console.log("false")            
-//             }
-    
-
-// }
-
-
-// verificaPalind('arara');
-
-let maiorValor = 0;
-
-function maiorIndice (array) {
-
-    for (let i  =  0; i < array.length; i += 1) {
-        if (array[i] > maiorValor) {
-            maiorValor = array[i]
+function verificaPalind (word) {
+    for (let i = 0; i < word.length; i += 1) {
+        if (word[i] == word[word.length -1 -i]) {
+            return true
+        } else {
+            return false
         }
     }
-        console.log("O indice do maior valor é " + array.indexOf(maiorValor))
 }
 
-maiorIndice[1, 4, 7, 9, 87, 4, 76, 43]
+console.log(verificaPalind('arara'));
