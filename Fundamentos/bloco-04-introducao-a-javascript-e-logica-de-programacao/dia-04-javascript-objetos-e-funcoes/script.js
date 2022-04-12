da4bc016736149692adb9106ff810c49bdeae8f8
Lucas <lucaschmain@gmail.com>
2022-04-12 12:40:30 -0300

    encontrando o indice do maior valor

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
@@ -1,11 +1,27 @@
-function verificaPalind (word) {
-    for (let i = 0; i < word.length; i += 1) {
-        if (word[i] == word[word.length -1 -i]) {
-            return true
-        } else {
-            return false
+// function verificaPalind (word) {
+//     for (let i = 0; i < word.length; i += 1) {
+//         if (word[i] == word[word.length -1 -i]) {
+//             return true
+//         } else {
+//             return false
+//         }
+//     }
+// }
+
+// console.log(verificaPalind('arara'));
+
+function maiorIndice (array) {
+    let maiorNumero = 0;
+    let indice = 0;
+    for (let index in array) {
+        if (array[index] > maiorNumero) {
+            maiorNumero = array[index]
+            indice = index
+            
         }
     }
+
+    return indice
 }
 
-console.log(verificaPalind('arara'));
+console.log(maiorIndice([2, 3, 6, 7, 10, 1, 98]))
