74a2026619e2dc2f6c2bda1d822acecdcae0ab40
Lucas <lucaschmain@gmail.com>
2022-05-06 19:16:32 -0300

    exercicio 4

--- Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,15 +1,28 @@
-<!DOCTYPE html>
-<body>
-  <div>
-    <span id ="text"></span>
-  </div>
-  <button id="button_test">click aqui!</button>
-</body>
-<script>
-  let clickCount = 0;
-  let textToDisplay = document.getElementById("text");
+const array = ["Android", "iOS", "Architecture", "Teach", "Run"];
 
-  document.getElementById("button_test")
-          .addEventListener("click", () => textToDisplay.innerHTML = clickCount += 1);
-</script>
-</html>
+function buildSkillsPhrase (paramOne) {
+    const fun1 = paramInner => (
+      `Tryber ${paramInner} aqui!
+
+      Tudo bem?`
+    );
+
+    let result = `${fun1(paramOne)}
+
+    Minhas cinco principais habilidades são:`;
+
+    array.forEach((skill, index) =>
+    result = `${result}
+
+    - ${skill}`);
+
+    result = `
+{result}
+
+    #goTrybe
+    `;
+
+    return result;
+}
+
+console.log(buildSkillsPhrase("Lucas"));
