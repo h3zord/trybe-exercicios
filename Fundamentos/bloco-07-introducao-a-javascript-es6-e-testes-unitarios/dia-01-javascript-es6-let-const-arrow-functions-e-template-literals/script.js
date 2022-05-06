64adefa5e7c926da02c1e2517ac4ec9e315d981c
Lucas <lucaschmain@gmail.com>
2022-05-06 19:06:48 -0300

    exercicio 2 parte 2

--- Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,15 +1,11 @@
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const factorial = number => {
+  let result = 1;
 
-const sortOddsAndEvens = () => {
-  oddsAndEvens[0] = 2;
-  oddsAndEvens[1] = 3;
-  oddsAndEvens[2] = 4;
-  oddsAndEvens[3] = 7;
-  oddsAndEvens[4] = 10;
-  oddsAndEvens[5] = 13;
+  for (let index = 2; index <= number; index += 1) {
+      result *= index;
+  }
 
-  return oddsAndEvens;
+  return result;
 }
 
-const sortedArray = sortOddsAndEvens();
-console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
+console.log(factorial(5));
